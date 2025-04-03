421f6fdae73c74e2c8f542b74468d45a2813cca1
Ravid Krisi <ravidkrisi22@gmail.com>
2025-04-03 13:28:20 +0700

    [feature] added logo to sign in screen

--- lib/features/auth/presentation/pages/sign_in_page.dart
+++ lib/features/auth/presentation/pages/sign_in_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:the_salon/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:the_salon/features/auth/presentation/bloc/auth_event.dart';
 
@@ -8,26 +11,58 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(width: double.infinity),
-        // logo
-        Text('Welcome to'),
-        Text(
-          'The Salon',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Spacer(),
 
-        // sign in with google btn
-        MaterialButton(
-          onPressed: () {
-            context.read<AuthBloc>().add(AuthSignInWithGoogle());
-          },
-          child: Text('sign in'),
-        ),
-      ],
+          SizedBox(width: double.infinity),
+          // logo
+          SvgPicture.asset(
+            'assets/icons/logo.svg',
+            width: MediaQuery.of(context).size.width * 0.5,
+          ),
+
+          SizedBox(height: 20),
+
+          // welcome text
+          Text('Welcome to,'),
+          Text(
+            'THE SALON',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+
+          Spacer(),
+          Spacer(),
+
+          // sign in with google btn
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              onPressed: () {
+                context.read<AuthBloc>().add(AuthSignInWithGoogle());
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(FontAwesomeIcons.google),
+                  SizedBox(width: 10),
+                  Text('Sign In With Google'),
+                ],
+              ),
+            ),
+          ),
+
+          SizedBox(height: 50),
+        ],
+      ),
     );
   }
 }
